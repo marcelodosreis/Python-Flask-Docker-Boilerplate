3a6c47da97e86943fc987d1c795aaa523479aec4
Marcelo dos Reis <marcelohrpaulo13@gmail.com>
2022-06-22 13:01:17 -0300

    chore: fix base of project

--- app.py
+++ app.py
@@ -15,13 +15,12 @@ app = Flask(__name__)
 # Routes
 @app.route("/", methods=["GET"])
 def hello_word():
-    return f'Hello, {os.getenv("POSTGRES_USER")}'
+    return 'Hello World'
 
 @app.route("/insert", methods=["POST"])
 def insert():
     userRepo = UserRepository()
-    body = request.json
-    userRepo.insert_user(body["name"])
+    userRepo.insert_user(request.json["name"])
 
     return "OK"
 
