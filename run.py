240a47704375687cb55f312020619a1c3054c641
Marcelo dos Reis <marcelohrpaulo13@gmail.com>
2022-06-22 12:43:35 -0300

    chore: add dotenvv

--- run.py
+++ run.py
@@ -1,13 +1,22 @@
-from crypt import methods
+# Core
 from flask import Flask, request
+
+# Repositories
 from src.repositories.users import UserRepository
 
+# Libs
+from dotenv import load_dotenv
+import os
 
+
+# Config App
+load_dotenv()
 app = Flask(__name__)
 
+# Routes
 @app.route("/", methods=["GET"])
 def hello_word():
-    return "Hello World!"
+    return f'Hello, ${os.getenv("POSTGRES_USER")}'
 
 @app.route("/insert", methods=["POST"])
 def insert():
